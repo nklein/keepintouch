8fa9be8a8d146ca5b44c90adaa5076d3512bdd6c
Patrick Stein <pat@nklein.com>
2023-01-30 15:45:24 -0600

    Added handler to automatically select dark/light mode

--- website/app/Views/footer.php
+++ website/app/Views/footer.php
@@ -1,4 +1,19 @@
     </main>
-    <script src="bootstrap.bundle.min.js" type="text/javascript" />
+    <script src="/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+     function setDarknessMode(dark) {
+         let body = document.querySelector('body');
+         if (dark) {
+             body.classList.replace('theme-light', 'theme-dark');
+         } else {
+             body.classList.replace('theme-dark', 'theme-light');
+         }
+     }
+     let matcher = window.matchMedia('(prefers-color-scheme: dark)');
+     matcher.addEventListener('change', function (e) {
+             setDarknessMode(e.matches);
+         });
+     setDarknessMode(matcher.matches);
+    </script>
   </body>
 </html>
